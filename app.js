6c23512ea1911419a45d5002fff16d715a8b6613
Karlaav05 <0251568@up.edu.mx>
2024-10-18 13:29:53 -0600

    Add front end for PokemonCards.html

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+var PORT = 8080;
+var app = express();
+app.use(express.static('public'));
+app.listen(PORT, () => {
+    console.log(`Server running`);
+});
+
+app.get("/", (req, res) => {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const _retfile = path.join(__dirname, 'PokemonCards.html');
+
+    res.sendFile(_retfile);
+});
